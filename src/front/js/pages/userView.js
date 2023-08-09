d3c002c9fe56817af1818baa49cca084dfd84ed8
mramirez092 <miguelramirez2210@gmail.com>
2023-08-09 07:54:06 +0000

    Saludo UserView

--- src/front/js/pages/userView.js
+++ src/front/js/pages/userView.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 
 export const UserView = () => {
-	return (
-		<div className="jumbotron">
-			<h1>Hola!</h1>
-            <br/>
-            <h4>Que se te antoja hoy! Pide por NaturalChoice</h4>
-		</div>
-	);
-};
+  const [userName, setUserName] = useState(""); // Estado para almacenar el nombre de usuario
+  const email = localStorage.getItem("email"); // Obtener el correo electrónico del almacenamiento local
 
+  return (
+    <div className="jumbotron">
+      <h1>Hola, {email}!</h1> {/* Mostrar el mensaje de bienvenida */}
+      <br />
+      <h4>¿Qué se te antoja hoy? Pide por NaturalChoice</h4>
+    </div>
+  );
+};
